69994fe26e33d547913e173730dfedc0b55cca28
Amjad <amjadmaqsuma@gmail.com>
2022-06-06 14:09:21 +0300

    Completed assignment

--- src/__tests__/user.test.js
+++ src/__tests__/user.test.js
@@ -1,7 +1,7 @@
 import { username, city, image } from "../data/user";
 
 test("username, city, and image are all exported", () => {
-  expect(username).toEqual("Liza");
-  expect(city).toEqual("New York");
+  expect(username).toEqual("Amjad");
+  expect(city).toEqual("Aleppo");
   expect(image).toEqual("https://i.imgur.com/mV8PQxj.gif");
 });
